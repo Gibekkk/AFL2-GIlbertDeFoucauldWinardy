30754aadb48d73fb35384890a1a560af5777bafd
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-07 08:38:19 +0800

    p

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('/cv', function () {
+    return view('cv');
+});
+Route::get('/projects', function () {
+    return view('projects');
+});
