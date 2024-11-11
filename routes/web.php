2cabff750080c294dc7934866e00f28cd3fd283b
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-11 11:37:06 +0800

    Revert "tes route"
    This reverts commit a0c86ad91e3bf1b379552bee97f48adf9ff4b97c.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,4 @@
 use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/{route?}', [RouteController::class, 'index']);
-Route::get('/' ,function(){
-    return view('welcome');
-});
+Route::get('/{route?}', [RouteController::class, 'index']);
