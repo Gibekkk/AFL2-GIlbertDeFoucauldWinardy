a0c86ad91e3bf1b379552bee97f48adf9ff4b97c
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-11 11:30:34 +0800

    tes route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{route?}', [RouteController::class, 'index']);
+// Route::get('/{route?}', [RouteController::class, 'index']);
+Route::get('/' ,function(){
+    return view('welcome');
+});
