2499ca041d019206885227fc835258d2dd31a94b
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-12 14:30:36 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,4 +2,22 @@
 
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
-require __DIR__ . '/../public/assets/*';
+require __DIR__ . '/../public/assets/CV.jpg';
+foreach (scandir(dirname(__DIR__ . '/../public/assets/css')) as $filename) {
+    $path = dirname(__DIR__ . '/../public/assets/css') . '/' . $filename;
+    if (is_file($path)) {
+        require $path;
+    }
+}
+foreach (scandir(dirname(__DIR__ . '/../public/assets/js')) as $filename) {
+    $path = dirname(__DIR__ . '/../public/assets/js') . '/' . $filename;
+    if (is_file($path)) {
+        require $path;
+    }
+}
+foreach (scandir(dirname(__DIR__ . '/../public/assets/images')) as $filename) {
+    $path = dirname(__DIR__ . '/../public/assets/images') . '/' . $filename;
+    if (is_file($path)) {
+        require $path;
+    }
+}
