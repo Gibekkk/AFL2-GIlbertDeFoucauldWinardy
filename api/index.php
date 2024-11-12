f3c7956e124369955c89706c9274c5f77f2267a2
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-12 14:48:55 +0800

    ??

--- api/index.php
+++ api/index.php
@@ -2,9 +2,3 @@
 
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
-foreach (scandir(dirname(__DIR__ . '/../public/assets/css')) as $filename) {
-    $path = dirname(__DIR__ . '/../public/assets/css') . '/' . $filename;
-    if (is_file($path)) {
-        require $path;
-    }
-}
