4338ec90688544b497d6107ff1d7ddc204e490ae
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-13 14:54:18 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RouteController;
 
 Route::get('/{route?}', [RouteController::class, 'index']);
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/CV', function () {
-    return view('cv');
-});
-Route::get('/projects', function () {
-    return view('projects');
-});
