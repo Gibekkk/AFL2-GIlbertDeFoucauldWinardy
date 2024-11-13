96d30644b9deaf5762cce00bf48a3b8628fdbfef
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-13 14:52:05 +0800

    ??

--- api/index.php
+++ api/index.php
@@ -2,4 +2,3 @@
 
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
-require __DIR__ . '/../app/Http/Controllers/RouteController.php';
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\RouteController;
+use App\Http\Controllers\RouteController;
 
 Route::get('/{route?}', [RouteController::class, 'index']);
 Route::get('/', function () {
